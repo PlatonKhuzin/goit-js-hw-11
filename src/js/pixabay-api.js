437056311dfb09e3d56be58fe3a119aad57f2f72
Platon kh <platonyaka@gmail.com>
2026-08-08 13:42:58 +0200

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_KEY = "57065525-8f77b836fab1fdfa97291f836";
-const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = '57065525-8f77b836fab1fdfa97291f836';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export function getImagesByQuery(query) {
-  return axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: query,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: true,
-    },
-  });
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
 }
